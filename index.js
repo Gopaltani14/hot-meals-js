1efccdde74051174656377b130e42ca0637300c5
Gopaltani14 <gopaltani14@gmail.com>
2023-09-14 14:52:01 +0530

    Order Item Changes

--- index.js
+++ index.js
@@ -195,6 +195,7 @@ function loginpagefood() {
     { img: "puttu", value: "Puttu", class: "puttu", price: 70 },
     { img: "vegrice", value: "Vegrice", class: "vegrice", price: 90 },
     { img: "Vegsoup", value: "Vegsoup", class: "Vegsoup", price: 30 },
+    
 
     { img: "halwa", value: "Halwa", class: "halwa", price: 40 },
     { img: "laddu", value: "Laddu", class: "laddu", price: 80 },
@@ -219,7 +220,7 @@ function loginpagefood() {
     popular += "<p><strong>" + e.value + "</strong></p>";
     popular +="<p>Price : <strong id='pricevalue-" +e.class +"'>" +e.price +"</strong></p>";
     popular +="<button key='" +e.class +"' id='btn-order-" +e.class +"' onclick='fnOrderItems(this)'>Order Now</button>";
-    popular +="<div id='btn-quantity-" +e.class +"' style='display:none'><button key='" +e.class +"' id='btn-add-" +e.class +"' onclick='fnAdd(this)'> + </button>";
+    popular +="<div id='btn-quantity-" +e.class +"' style='display:none;justify-content:space-around;align-items:center;gap:15px'><button key='" +e.class +"' id='btn-add-" +e.class +"' onclick='fnAdd(this)'> + </button>";
     popular += "<p id='p-count-" + e.class + "'>1</p>";
     popular +="<button key='" +e.class +"' id='btn-sub-" +e.class +"' onclick='fnSub(this)'> - </button></div>";
     popular += "</div>";
@@ -235,31 +236,57 @@ function fnOrderItems(item) {
   document.querySelector("#" + item.id).style.display = "none";
   document.querySelector(
     "#btn-quantity-" + item.getAttribute("key")
-  ).style.display = "block";
+  ).style.display = "flex";
   OrderedItems.push({
     Item: item.getAttribute("key"),
     count: 1,
     img: item.getAttribute("key"),
-    price: document.getElementById("pricevalue-" + item.getAttribute("key"))
-      .innerHTML,
+    price: document.getElementById("pricevalue-" + item.getAttribute("key")).innerHTML,
+    total: document.getElementById("pricevalue-" + item.getAttribute("key")).innerHTML,
   });
   itemCount++;
   document.querySelector("#cart-count").innerHTML = itemCount;
+
 }
+
+
 
 function fnAdd(item) {
   //getting the innerHTML value of count and assign it to the variable
-  let count = document.getElementById("p-count-" + item.getAttribute("key")
-  ).innerHTML;
+  let count = document.getElementById("p-count-" + item.getAttribute("key")).innerHTML;
   count = parseInt(count) + 1;
-  document.getElementById("p-count-" + item.getAttribute("key")).innerHTML =
-    count;
-
+  
   let index = OrderedItems.findIndex((e) => e.Item == item.getAttribute("key")); // To get the index from the orderItems array for the specific dish
   OrderedItems[index].count = count;
+  
+  //assigning and updating intial increment count to the object and setting it to the cart count label
+  document.getElementById("p-count-" + item.getAttribute("key")).innerHTML =count;
+  OrderedItems[index].count=count;
+  OrderedItems[index].total=parseInt(OrderedItems[index].price)*parseInt(OrderedItems[index].count);
+ 
+  //updating the totalitem price when increment
+  let itemindexprice=OrderedItems[index].price;
+// console.log(typeof(itemindexprice));
+let updatedTotalItemprice=parseInt(itemindexprice)*parseInt(OrderedItems[index].count);
+document.getElementById("itemTotal-"+item.getAttribute("key")).innerHTML=updatedTotalItemprice;
+OrderedItems[index].total=updatedTotalItemprice;//updating the total value through index 
+  
+  //cart-items count value changing
+  let cartcount = document.getElementById("p-cart-count-" + item.getAttribute("key")).innerHTML;
+  cartcount = parseInt(cartcount) + 1;
+  document.getElementById("p-cart-count-" + item.getAttribute("key")).innerHTML =cartcount;
+
+
+ 
+   //updating the sum of total when increment 
+   let overAllTotalVariable=OrderedItems.reduce((a, b) => ({total: parseInt(a.total) + parseInt(b.total)}))
+   console.log(typeof(overAllTotalVariable));
+   document.getElementById("total-div").innerHTML=`<p>Sum  :</p><h3>${overAllTotalVariable.total}</h3>`;
+ 
   itemCount++;
   document.querySelector("#cart-count").innerHTML = itemCount;
 }
+
 
 function fnSub(item) {
   let count = document.getElementById(
@@ -269,30 +296,69 @@ function fnSub(item) {
   document.getElementById("p-count-" + item.getAttribute("key")).innerHTML =
     count;
 
+  //cart-items count value changing
+  let cartcount = document.getElementById("p-cart-count-" + item.getAttribute("key")).innerHTML;
+  cartcount = parseInt(cartcount) - 1;
+  document.getElementById("p-cart-count-" + item.getAttribute("key")).innerHTML =cartcount;
+
   let index = OrderedItems.findIndex((e) => e.Item == item.getAttribute("key"));
   OrderedItems[index].count = count;
+
+
+//updating the totalitem price when increment
+let itemindexprice=OrderedItems[index].price;
+console.log(typeof(itemindexprice));
+let updatedTotalItemprice=parseInt(itemindexprice)*parseInt(OrderedItems[index].count);
+document.getElementById("itemTotal-"+item.getAttribute("key")).innerHTML=updatedTotalItemprice;
+OrderedItems[index].total=updatedTotalItemprice;//updating the total value through index 
+ 
+//updating the sum of total when increment 
+let overAllTotalVariable=OrderedItems.reduce((a, b) => ({total: parseInt(a.total) + parseInt(b.total)}))
+console.log(typeof(overAllTotalVariable));
+document.getElementById("total-div").innerHTML=`<p>Sum  :</p><h3>${overAllTotalVariable.total}</h3>`;
+
   itemCount--;
   document.querySelector("#cart-count").innerHTML = itemCount;
 }
 
 //cart displaying
 function displayCart() {
+  debugger
   document.querySelector(".cart-popup").style.display = "block";
 
   console.log(OrderedItems);
-  cartItems = "";
+  let cartItems = "";
   OrderedItems.map((i) => {
-    cartItems += '<div id="card-item-div">';
+    cartItems += '<div key="' +i.Item +'" id="cart-item-div">';
+    cartItems += '<div key="' +i.Item +'" onclick="fnDelete(this)" class="cart-item-delete" id="card-item-delete"><i class="fa-solid fa-trash"></i></div>';
     cartItems += '<img src="' + i.img + '.png" alt="' + i.Item + 'image" />';
     cartItems += "<p>" + i.Item + "</p>";
     cartItems += "<p>" + i.price + "</p>";
-    cartItems += '<div id="quantity-div">';
-    cartItems += '<button key="' +i.Item +' id="btn-sub-' +i.Item +'" onclick="cartfnSub(this)">-</button>';
-    cartItems += "<p id='p-count-" + i.Item + "'>" + i.count + "</p>";
-    cartItems += '<button key="' +i.Item +' id="btn-add-' +i.Item +'" onclick="cartfnAdd(this)">+</button>';
+    cartItems += '<div id="btn-quantity-' +i.Item +'" style="display:flex;justify-content:center;align-items:center;gap:10px">';
+    cartItems += '<button key="' +i.Item +'" id="btn-sub-' +i.Item +'" onclick="fnSub(this)" style="padding:3px 14px;background:green;color:white;font-size:19px;border:none;border-radius:10px">-</button>';
+    cartItems += "<p id='p-cart-count-" + i.Item + "'>" + i.count + "</p>";
+    cartItems += '<button key="' +i.Item +'" id="btn-add-' +i.Item +'" onclick="fnAdd(this)" style="padding:3px 14px;background:green;color:white;font-size:19px;border:none;border-radius:10px">+</button>';
     cartItems += "</div>";
-    cartItems += "<p>0000</p>";
+    cartItems += '<p id="itemTotal-'+i.Item+'">'+i.total+'</p>';
     cartItems += "</div>";
   });
-  document.getElementById("card-items-div").innerHTML = cartItems;
+  document.getElementById("cart-item-display-div").innerHTML = cartItems;
+
+  //displaying sum of total in the cart popup
+  let overAllTotalVariable=OrderedItems.reduce((a, b) => ({total: parseInt(a.total) + parseInt(b.total)}))
+  console.log(typeof(overAllTotalVariable));
+  document.getElementById("total-div").innerHTML=`<p>Sum  :</p><h3>${overAllTotalVariable.total}</h3>`;
+
+}
+
+function fnDelete(item){
+  //finding index of that particular item's
+  OrderedItems=OrderedItems.filter((e)=>e.Item!=item.getAttribute("key"));
+  // console.log(index);
+  displayCart()
+   //displaying sum of total in the cart popup
+   let overAllTotalVariable=OrderedItems.reduce((a, b) => ({total: parseInt(a.total) + parseInt(b.total)}))
+   console.log(typeof(overAllTotalVariable));
+   document.getElementById("total-div").innerHTML=`<p>Sum  :</p><h3>${overAllTotalVariable.total}</h3>`;
+ 
 }
